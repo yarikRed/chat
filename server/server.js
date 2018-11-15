29c8b17a46a13f4c2c0a1ab8dbccaa9a39fbfddf
yarik <yarik.red@gmail.com>
2018-11-15 19:25:18 +0200

    sockets added

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const app = express();
 const nunjucks = require('nunjucks');
+const server = require('http').Server(app);
+const io = require('socket.io')(server, {serveClient: true});
 
 nunjucks.configure('./client/views', {
    autoescape: true,
@@ -15,6 +17,23 @@ app.get("/", (req, res) => {
     res.render('index.html', {date: new Date()});
 });
 
-app.listen(7777, "0.0.0.0", () => {
+io.on('connection', function (socket) {
+
+    socket.emit('connected', "Connected!!!!!");
+
+    socket.join('all');
+
+    socket.on('msg', content => {
+        const obj = {
+            date: new Date(),
+            content: content,
+            username: socket.id
+        }
+        socket.emit("message", obj);
+        socket.to('all').emit("message", obj);
+    });
+});
+
+server.listen(7777, "0.0.0.0", () => {
     console.log("Server started on port 7777");
 });
