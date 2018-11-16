81cc394cde82c67a2b3ea704978817e703fd7a5f
yarik <yarik.red@gmail.com>
2018-11-16 19:40:12 +0200

    authentication

---
+++ server/config.js
@@ -0,0 +1,20 @@
+"use strict";
+
+
+function ExtractJwt (req){
+    let token = null;
+    if (req.cookies && req.cookies.token != void(0)) {
+        token = req.cookies['token'];
+    }
+    return token;
+}
+
+module.exports = {
+  jwt: {
+          jwtFromRequest: ExtractJwt,
+          secretOrKey: 'secret'
+  },
+
+  expiresIn: '1 day'
+
+};
--- server/server.js
+++ server/server.js
@@ -6,6 +6,18 @@ const nunjucks = require('nunjucks');
 const server = require('http').Server(app);
 const io = require('socket.io')(server, {serveClient: true});
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const passport = require('passport');
+const { Strategy } = require('passport-jwt');
+
+const { jwt } = require('./config');
+
+passport.use(new Strategy(jwt, function(jwt_payload, done){
+    if (jwt_payload != void(0)) return done(false, jwt_payload);
+    done();
+}));
 
 mongoose.connect('mongodb://admin:zaq12wsx@ds157964.mlab.com:57964/notes', {useNewUrlParser: true});
 mongoose.Promise = require('bluebird');
@@ -15,14 +27,16 @@ nunjucks.configure('./client/views', {
    express: app
 });
 
-app.use('/assets', express.static('./client/public'));
+app.use(bodyParser.urlencoded({extend: false}));
 
-app.get("/", (req, res) => {
-    res.render('index.html', {date: new Date()});
-});
+app.use(bodyParser.json());
+
+app.use(cookieParser());
+
+require('./router')(app);
 
 require('./sockets')(io);
 
-server.listen(7777, "0.0.0.0", () => {
+server.listen(7777, () => {
     console.log("Server started on port 7777");
 });
